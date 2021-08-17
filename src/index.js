2bed5a9ec84de9005d33dc4403420dbae671f41a
anapdh <ana_d_hubner@hotmail.com>
2021-08-17 14:33:05 -0300

    Add html format for list

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ function component() {
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+  element.classList.add('hello');
 }
 
 document.body.appendChild(component());
