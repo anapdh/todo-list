acb9f93f0be1b1b230fb7a99fc79c2bbf3bf4728
anapdh <ana_d_hubner@hotmail.com>
2021-08-17 16:54:39 -0300

    Add function to create todos and default todos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const todoList = document.querySelector('.todo-list');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const myList = [
+  {
+    description: 'wash car',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'homework',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'English class',
+    completed: false,
+    index: 3,
+  },
+];
+
+function todoTasks() {
+  myList.forEach((todo) => {
+    const newTask = `
+    <li class="todo-item item" data-index="${todo.index}">
+      <input class="todo-checkbox" type="checkbox" value="${todo.index}">
+      <input class="todo-text" type="text" value="${todo.description}">
+    </li>
+    `;
+
+    todoList.innerHTML += newTask;
+  });
 }
 
-document.body.appendChild(component());
+window.addEventListener('load', () => todoTasks());
