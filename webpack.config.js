5344ef22da9212bd6e4b734a5756af3e94119ab3
anapdh <ana_d_hubner@hotmail.com>
2021-08-17 14:16:23 -0300

    Add loades for css and images

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,20 @@ module.exports = {
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
