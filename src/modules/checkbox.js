c5a775de455ce2720d7b338fdb98cfbfb3d9b442
anapdh <ana_d_hubner@hotmail.com>
2021-08-18 19:19:50 -0300

    Fix function to change todo to completed in localStorage

--- src/modules/checkbox.js
+++ src/modules/checkbox.js
@@ -1,18 +1,20 @@
 export default function checkTodos(todo) {
   let todos = JSON.parse(localStorage.getItem('myList'));
-  const todoN = todo.children[1].innerHTML;
-  const todoIndex = todos.findIndex((item) => item.value === todoN.value);
+  // const todoN = todo.children[1].innerHTML;
+  const todoIndex = todos.findIndex(item => item.description === todo.children[1].value);
   todos[todoIndex].completed = !todos[todoIndex].completed;
   localStorage.setItem('myList', JSON.stringify(todos));
 }
 
 const list = document.querySelector('.todo-list');
 
-list.addEventListener('click', (e) => {
+list.addEventListener('change', (e) => {
+  let todos = JSON.parse(localStorage.getItem('myList'));
   const item = e.target;
   if (item.classList[0] === 'todo-check') {
     const item2 = item.parentElement;
     item2.classList.toggle('completed-task');
+    localStorage.setItem('myList', JSON.stringify(todos));
     checkTodos(item2);
   }
 });
